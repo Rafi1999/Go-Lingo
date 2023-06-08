633a99f3c435f125bb6a8491735140b80c6aaff3
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-06-08 20:46:48 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet";
+
+const Home = () => {
+    return (
+        <div>
+        <Helmet>Go-Lingo | Home</Helmet>
+        <h2>Welcome Home</h2>            
+        </div>
+    );
+};
+
+export default Home;
